cdbb737850624cd231e87fda57269340b6914c54
Dongeg <821220992@qq.com>
2016-07-08 20:01:34 +0800

    组件复用

--- js/script.js
+++ js/script.js
@@ -7,25 +7,10 @@ var MyForm=React.createClass({
 			checked:true
 		};
 	},
-	handlePhoneNumberChange:function(event){
-		this.setState({
-			phoneNumber:event.target.value
-		});
-	},
-	handlePassWordChange:function(event){
-		this.setState({
-			passWord:event.target.value
-		})
-	},
-	handleVerifyCodeChange:function(event){
-		this.setState({
-			verifyCode:event.target.value
-		})		
-	},
-	handleCheckboxChange:function(event){
-		this.setState({
-			checked:event.target.checked
-		})			
+	handleChange:function(name,event){
+		var newState={};
+		newState[name]= name=="checked"?event.target.checked:event.target.value;
+		this.setState(newState);
 	},
 	submitHandler:function(event){
             event.preventDefault();
@@ -37,21 +22,21 @@ var MyForm=React.createClass({
 				<p>
 				  <label>手机号</label> 
 				  <input type="text" placeholder="用于登陆和找回密码" maxlength="11" autocomplete="off" className="phone" 
-				         value={this.state.phoneNumber} onChange={this.handlePhoneNumberChange} />
+				        pattern="[0-9]{11}" title="请输入11位手机号"  value={this.state.phoneNumber} onChange={this.handleChange.bind(this,"phoneNumber")} />
 				</p>
 				<p><label>密码</label> 
 				   <input type="password" placeholder="密码" maxlength="20" autocomplete="off"  className="password"
-				          value={this.state.passWord} onChange={this.handlePassWordChange} />
+				          value={this.state.passWord} onChange={this.handleChange.bind(this,"passWord")} />
 				</p>
 				<p>
 					<label>验证码</label>
 					<input type="text" placeholder="验证码" maxlength="6" autocomplete="off" className="verifyCode" 
-					       value={this.state.verifyCode} onChange={this.handleVerifyCodeChange} />
+					       value={this.state.verifyCode} onChange={this.handleChange.bind(this,"verifyCode")} />
 					<input type="button" value="获取短信验证码" className="getVerifyCode" />
 				</p>
 				<p>
 					<input type="checkbox" autocomplete="off" checked="checked" className="checkbox"
-					       value={this.state.checked} onChange={this.handleCheckboxChange} />
+					       value={this.state.checked} onChange={this.handleChange.bind(this,"cheched")} />
 					<span className="read">阅读并接受<a href="#">《百度用户协议》</a></span>
 				</p>
 				<p>
